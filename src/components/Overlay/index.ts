9302c25fe89d47fd93ebb2e66d44267c8dbb12d3
Novik Dmytro <dinovik@rambler.ru>
2023-08-13 05:00:49 +0300

    ECOMM-55 feat: add overlay web component

---
+++ src/components/Overlay/index.ts
@@ -0,0 +1,60 @@
+import { Dispatch } from 'redux';
+import { connect } from 'webcomponents-redux';
+import ElementHTML from './index.html';
+import createNodeFromHtml from '../../utils/createNodeFromHtml';
+import { StateLocation } from '../../types';
+import store from '../Store/store';
+
+export default class CustomOverlay extends HTMLElement {
+  public $element: HTMLElement | null;
+
+  private node: Node | null;
+
+  constructor() {
+    super();
+    this.node = createNodeFromHtml(ElementHTML);
+    if (this.node?.firstChild instanceof HTMLElement) this.$element = this.node.firstChild;
+    else this.$element = null;
+  }
+
+  private connectedCallback(): void {
+    this.attachShadow({ mode: 'open' });
+    if (this.node) this.shadowRoot?.append(this.node);
+    this.setAttribute('active', 'false');
+  }
+
+  private disconnectedCallback(): void {}
+
+  private attributeChangedCallback(attributeName: string, oldValue: string, newValue: string): void {
+    if (attributeName === 'active') {
+      switch (newValue) {
+        case 'true':
+          this.$element?.classList.add('active');
+          console.log('overlay added');
+          break;
+        case 'false':
+          this.$element?.classList.remove('active');
+          console.log('overlay removed');
+          break;
+        default:
+          break;
+      }
+    }
+  }
+
+  private mapStateToProps(oldState: StateLocation, newState: StateLocation): void {}
+
+  private mapDispatchToProps(dispatch: Dispatch): { [index: string]: () => ReturnType<Dispatch> } {
+    return {
+      action: () => dispatch({ type: 'ACTION' }),
+    };
+  }
+
+  private adoptedCallback(): void {}
+
+  private static get observedAttributes(): string[] {
+    return ['active'];
+  }
+}
+connect(CustomOverlay, store);
+window.customElements.define('custom-overlay', CustomOverlay);
