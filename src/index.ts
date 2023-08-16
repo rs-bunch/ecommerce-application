996fe17f5b1a954d79ce345f346d965fbb97f51d
maxsimusprime <96006023+maxsimusprime@users.noreply.github.com>
2023-08-16 23:42:22 +0300

    ECOMM-46 chore: install toastify-js

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import './styles/typography.scss';
+import './styles/toastify.css';
 import { connect } from 'webcomponents-redux';
 import { bootstrap } from './styles/styles';
 import store from './components/Store/store';
@@ -8,6 +9,7 @@ import ShopHeader from './components/Header/Header';
 import Login from './components/login/Login';
 
 document.adoptedStyleSheets = [bootstrap];
+
 const router: Router = new Router(store);
 
 connect(CustomOverlay, store);
