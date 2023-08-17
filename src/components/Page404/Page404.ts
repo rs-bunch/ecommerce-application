a9250d081522d23c013d214ad37472904e52e1c7
Novik Dmytro <dinovik@rambler.ru>
2023-08-17 17:51:56 +0300

    ECOMM-59 feat: add a 404 page

---
+++ src/components/Page404/Page404.ts
@@ -0,0 +1,56 @@
+import { Dispatch } from 'redux';
+import ElementHTML from './index.html';
+import stylesheet from './styles.module.scss';
+import { bootstrap } from '../../styles/styles';
+import createNodeFromHtml from '../../utils/createNodeFromHtml';
+import { RootState } from '../Store/store';
+
+export default class Page404 extends HTMLElement {
+  public $element: HTMLElement | null;
+
+  public node: Node | null;
+
+  constructor() {
+    super();
+    this.node = createNodeFromHtml(ElementHTML);
+    if (this.node && this.node.firstChild instanceof HTMLElement) this.$element = this.node.firstChild;
+    else this.$element = null;
+  }
+
+  private connectedCallback(): void {
+    this.attachShadow({ mode: 'open' });
+    if (this.$element) this.shadowRoot?.appendChild(this.$element);
+    if (this.shadowRoot) this.shadowRoot.adoptedStyleSheets = [stylesheet, bootstrap];
+  }
+
+  private disconnectedCallback(): void {}
+
+  private attributeChangedCallback(attributeName: string, oldValue: string | null, newValue: string | null): void {
+    if (attributeName === 'location') {
+      console.log(newValue);
+      if (newValue) this.$element?.setAttribute('location', newValue);
+    }
+  }
+
+  // redux state change observer
+  private mapStateToProps(oldState: RootState, newState: RootState): void {
+    if (!oldState) return;
+    if (oldState.location.location !== newState.location.location)
+      this.attributeChangedCallback('location', oldState.location.location, newState.location.location);
+    console.log(newState);
+    console.log(newState.location);
+  }
+
+  // redux dispath action
+  private mapDispatchToProps(dispatch: Dispatch): { [index: string]: () => ReturnType<Dispatch> } {
+    return {
+      action: () => dispatch({ type: 'ACTION' }),
+    };
+  }
+
+  private adoptedCallback(): void {}
+
+  private static get observedAttributes(): string[] {
+    return ['name'];
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ import Router from './components/Router/Router';
 import CustomOverlay from './components/Overlay/Overlay';
 import ShopHeader from './components/Header/Header';
 import Login from './components/login/Login';
+import Page404 from './components/Page404/Page404';
 
 document.adoptedStyleSheets = [bootstrap];
 
@@ -20,3 +21,6 @@ connect(ShopHeader, store);
 customElements.define('nav-element', ShopHeader);
 
 customElements.define('login-form', Login);
+
+connect(Page404, store);
+customElements.define('error-element', Page404);
