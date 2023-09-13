ee53d0ddf7a8142ea091f1abbe22f6fc14471f89
maxsimusprime <96006023+maxsimusprime@users.noreply.github.com>
2023-09-13 19:33:23 +0300

    ECOMM-153 feat: add email and password tests

--- src/tests/redux.test.ts
+++ src/tests/redux.test.ts
@@ -1,6 +1,7 @@
 import { ProductData } from '@commercetools/platform-sdk';
 import { initLocation } from '../components/Store/locationSlice';
 import { selectProduct, selectProductVariant } from '../components/Store/productSlice';
+import { initAuth, logout } from '../components/Store/authSlice';
 import store from '../components/Store/store';
 
 const productMock = {
@@ -12,6 +13,18 @@ const productMock = {
   },
   variants: [],
   searchKeywords: {},
+};
+
+const state = {
+  id: 'id12345',
+  version: 1,
+  createdAt: '2222-11-11',
+  lastModifiedAt: '2222-11-11',
+  email: 'email@domain.com',
+  addresses: [],
+  isEmailVerified: false,
+  authenticationMode: 'email',
+  inProgress: false,
 };
 
 describe('Testing Location', () => {
@@ -33,5 +46,26 @@ describe('Testing ProductData', () => {
   it('Try change variant', () => {
     store.dispatch(selectProductVariant({ id: 2 }));
     expect(store.getState().product.id).toEqual(2);
+  });
+});
+
+describe('Testing authSlice.actions', () => {
+  it('Try set up initAuth', () => {
+    store.dispatch(initAuth(state));
+    expect(store.getState().auth.id).toEqual('id12345');
+    expect(store.getState().auth.version).toEqual(1);
+    expect(store.getState().auth.createdAt).toEqual('2222-11-11');
+    expect(store.getState().auth.lastModifiedAt).toEqual('2222-11-11');
+    expect(store.getState().auth.email).toEqual('email@domain.com');
+    expect(store.getState().auth.authenticationMode).toEqual('email');
+  });
+  it('Try to logout', () => {
+    store.dispatch(logout());
+    expect(store.getState().auth.id).toEqual('');
+    expect(store.getState().auth.version).toEqual(0);
+    expect(store.getState().auth.createdAt).toEqual('');
+    expect(store.getState().auth.lastModifiedAt).toEqual('');
+    expect(store.getState().auth.email).toEqual('');
+    expect(store.getState().auth.authenticationMode).toEqual('');
   });
 });
